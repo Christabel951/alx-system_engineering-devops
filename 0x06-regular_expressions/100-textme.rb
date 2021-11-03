177ae583ce3effd3124754ee226340ff1831e7f2
Christabel951 <crkomo@gmail.com>
2021-11-03 18:11:24 +0000

    format output with print

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)([A-Za-z]+|\+?[0-9]+)/).join, ARGV[0].scan(/(?<=to:)([A-Za-z]+|\+?[0-9]+)/).join, ARGV[0].scan(/(?<=flags:)(\-?[0-9]:\-?[0-9]:\-?[0-9]:\-?[0-9]:\-?[0-9])/)
+sender = ARGV[0].scan(/(?<=from:)([A-Za-z]+|\+?[0-9]+)/).join
+receiver = ARGV[0].scan(/(?<=to:)([A-Za-z]+|\+?[0-9]+)/).join
+flags = ARGV[0].scan(/(?<=flags:)(\-?[0-9]:\-?[0-9]:\-?[0-9]:\-?[0-9]:\-?[0-9])/).join
+print sender + "," + receiver + "," + flags + "\n\n"
