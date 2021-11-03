d3209e10fe84da6d3754420b7da868dc98fc3b6e
Christabel951 <crkomo@gmail.com>
2021-11-03 03:14:12 +0000

    use given base code snippet

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,2 @@
 #!/usr/bin/env ruby
-#The regular expression must match School
-if ARGV.length == 1
-    for i in ARGV
-        puts /School/.match(i)
-    end
-end
+puts ARGV[0].scan(/School/).join
